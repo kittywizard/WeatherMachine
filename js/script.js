33ee6ae3a16be2b626775b18a4972763dd0ff620
marleigh <volcana@gmail.com>
2017-03-07 18:59:19 -0800

    Updating

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function (){
 
+//declare variables
+var $setTemp = $(".temp");
+var $setLocation = $(".location");
+
+//call API
 $.ajax({
     type: 'GET',
     url: 'http://api.wunderground.com/api/f9ee0f522276d317/conditions/q/wa/seattle.json',
@@ -8,11 +13,17 @@ $.ajax({
       var temp_f = data.current_observation.temp_f;
       var temp_c = data.current_observation.temp_c;
       var conditions = data.current_observation.weather;
+      var location = data.current_observation.display_location.full;
 
         //a way to determine what icon gets displayed based on current temperature and conditions
         //just text to start
         console.log(temp_f + " &deg; F");
         console.log(temp_c + " &deg; C");
+        console.log(conditions + location);
+
+        ($setTemp).html("<p>" + temp_f + "</p>");
+        ($setLocation).html("<p> &mdash; " + location + "&deg; F</p>");
+
         }
     });
 });
