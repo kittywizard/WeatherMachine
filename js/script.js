258d4b60d86695b33a1c3f94abffcda48ccf615f
marleigh <volcana@gmail.com>
2017-03-25 16:29:51 -0700

    New Zipcode Update

--- js/script.js
+++ js/script.js
@@ -4,75 +4,66 @@ $(document).ready(function (){
 var $setTemp = $(".temp");
 var $setLocation = $(".location");
 var $setConditions = $(".conditions");
+var input = 0;
 
-//call API
-$.ajax({
-    type: 'GET',
-    url: 'http://api.wunderground.com/api/f9ee0f522276d317/conditions/q/wa/seattle.json',
-    success: function (data) {
-      //variables for the temps and conditions i need
+//listen for a click on the submit button to get data
+var getZipcode = document.getElementById('subButton');
+subButton.addEventListener('click', getZip, false);
 
-      //a third temp variable to determine which one is being displayed
-      var temp = data.current_observation.temp_f;
+//runs this function when the button is clicked
+function getZip() {
+  //save user input as variable
+  input = document.getElementById("zipField").value;
 
-      var temp_f = data.current_observation.temp_f;
-      var temp_c = data.current_observation.temp_c;
-      var conditions = data.current_observation.weather;
-      var location = data.current_observation.display_location.full;
+//zipcode not the right length, bad user
+  if (input.length !== 5) {
+    //alert("Please enter a 5 digit zipcode.");
+    console.log("Enter a 5 digit zipcode error");
+  }
+  console.log(input);
 
-      //display info
-      ($setTemp).html("<p>" + temp_f + "&deg; F</p>");
-      ($setLocation).html("<p>" + location + "</p>");
+  //start the API call!
+  $.ajax({
+      type: 'GET',
+      url: 'http://api.wunderground.com/api/f9ee0f522276d317/conditions/q/' + input + '.json',
+      success: function (data) {
 
 
-        /* weather icons from font awesome:
-        sun, moon, cloud, snowflake, umbrella
-        */
+        //a third temp variable to determine which one is being displayed
+        var temp = data.current_observation.temp_f;
 
-        switch (conditions) {
-          case 'Overcast':
-            ($setConditions).html("<i class='fa fa-cloud' aria-hidden='true'></i> <p>" + conditions);
-            break;
+        var temp_f = data.current_observation.temp_f;
+        var temp_c = data.current_observation.temp_c;
+        var conditions = data.current_observation.weather;
+        var location = data.current_observation.display_location.full;
+        var $button = $("#convert");
 
-          /*case 'Clear':
-          //check current time. if clear > display a sun or moon
-            if (CHECK FOR CURRENT TIME : COMPARE TO TIME) {
-              ($setConditions).html("<i class='fa fa-sun' aria-hidden='true'></i> <p>" + conditions);
-            }
-            else if (CHECK TO SEE IF ITS NIGHT TIME) {
-              ($setConditions).html("<i class='fa fa-moon' aria-hidden='true'></i> <p>" + conditions);
-            }
-            break;
+        //display info
+        ($setTemp).html("<p>" + temp_f + "&deg; F</p>");
+        ($setLocation).html("<p>" + location + "</p>");
+        ($setConditions).html("<p>" + conditions + "</p>");
 
-          case expression:
+                //click the button to change the temperature from F to C and back again
 
-            break;
+                $("#convert").on('click', function() {
+                  if(temp == temp_f) {
+                    temp = temp_c;
+                    ($setTemp).html("<p>" + temp_c + "&deg; C</p>");
 
-          case expression:
+                  }
+                  else if (temp == temp_c){
+                    temp = temp_f;
+                    ($setTemp).html("<p>" + temp_f + "&deg; F</p>");
 
-            break;*/
-        }
-
-        //click the button to change the temperature from F to C and back again
-
-        $("#convert").on('click', function() {
-          if(temp == temp_f) {
-            temp = temp_c;
-            ($setTemp).html("<p>" + temp_c + "&deg; C</p>");
-
-          }
-          else if (temp == temp_c){
-            temp = temp_f;
-            ($setTemp).html("<p>" + temp_f + "&deg; F</p>");
-
-          }
+                  }
 
 
 
-        });
-      }
-    });
+                });
+              }
+            });
+}
 
 
 
-});
+      });
